2ae79778e4509954ee258a32cc671a26031f4649
ibrahim <ibrahim.asif.mirza@gmail.com>
2021-05-02 13:15:32 +0200

    feat(project): Update assets

--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -7,6 +7,13 @@ public class BasicMovement : MonoBehaviour
 
     public Animator animator;
     public float movementSpeed;
+    public float jumpForce;
+    
+    public float groundCheckRadius;
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+    private bool isGrounded;
+    private bool canJump;
 
     private Rigidbody2D rb;
     private float movementInputDirection;
@@ -15,6 +22,8 @@ public class BasicMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        this.isGrounded = false;
     }
 
     // Update is called once per frame
@@ -25,25 +34,81 @@ public class BasicMovement : MonoBehaviour
 
         if (movementInputDirection < 0)
         {
-            characterScale.x = -1;
+            characterScale.x = -5;
         }
 
         if (movementInputDirection > 0)
         {
-            characterScale.x = 1;
+            characterScale.x = 5;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            performJump();
         }
 
         transform.localScale = characterScale;
         // transform.position = transform.position + horizontal * Time.deltaTime;
         
-
-        animator.SetFloat("Speed", Mathf.Abs(movementInputDirection));
+        updateAnimations();
+        checkIfCanJump();
+        
 
         Debug.Log(movementInputDirection);
     }
 
+    private void updateAnimations() {
+        animator.SetFloat("Speed", Mathf.Abs(movementInputDirection));
+        animator.SetBool("isGrounded", isGrounded);
+        animator.SetFloat("yVelocity", rb.velocity.y);
+    }
+
+    private void checkIfCanJump() {
+
+        if (isGrounded && rb.velocity.y <= 0) {
+            this.canJump = true;
+        } else {
+            this.canJump = false;
+        }
+
+    }
+
+    private void performJump() {
+        // detectGroundTouch();
+        // this.isJumping = true;
+        // animator.SetBool("Jump", this.isJumping);
+        if (canJump) {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+        
+    }
+
     private void FixedUpdate()
     {
+        // if (this.isJumping)
+        // {
+        //     rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        //     // this.isJumping = false;
+        //     // animator.SetBool("Jump", this.isJumping);
+        // }
+        // if (!this.isJumping) {
+        //     animator.SetBool("Jump", this.isJumping);
+        // if (rb.velocity.y <= 0 && !this.isJumping) {
+        //     rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        // }
         rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
+        // }
+
+        CheckSurroundings();
+    }
+
+    private void CheckSurroundings()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+        Debug.Log(isGrounded);
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
